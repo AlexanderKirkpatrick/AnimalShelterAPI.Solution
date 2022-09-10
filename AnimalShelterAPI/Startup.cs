5eb591c66a82fcae94ec0348d676f3df30aeae0c
Alexander <knibz@protonmail.com>
2022-09-10 15:50:32 -0400

    add entity using and fix using model name

--- AnimalShelterAPI/Startup.cs
+++ AnimalShelterAPI/Startup.cs
@@ -10,7 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
+using Microsoft.EntityFrameworkCore;
+using AnimalShelterAPI.Models;
 
 namespace AnimalShelterAPI
 {
