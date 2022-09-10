05770465363a082844358037ccf34b769148c2d0
Alexander <knibz@protonmail.com>
2022-09-10 13:05:24 -0400

    add paramaters for pagination

---
+++ AnimalShelterAPI/Models/PaginationFilter.cs
@@ -0,0 +1,28 @@
+namespace AnimalShelterAPI.Models
+{
+  public class PaginationFilter
+  {
+    public int PageNumber { get; }
+    public int PageSize { get; }
+
+    public PaginationFilter()
+    {
+      this.PageNumber = 1;
+      this.PageSize = 10;
+    }
+
+    public PaginationFilter(int pageNumber, int pageSize)
+    {
+      if(pageSize > 50)
+      {
+        pageSize = 50;
+      } else if (pageSize <= 0)
+      {
+        pageSize = 10;
+      }
+
+      this.PageNumber = pageNumber;
+      this.PageSize = pageSize;
+    }
+  }
+}
