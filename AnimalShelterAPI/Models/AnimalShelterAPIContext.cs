d438175754d417405f8098c7404bed4c3ec6acdb
Alexander <knibz@protonmail.com>
2022-09-10 18:23:04 -0400

    edit seeds to remove duplicates, add a few more for pagination check

--- AnimalShelterAPI/Models/AnimalShelterAPIContext.cs
+++ AnimalShelterAPI/Models/AnimalShelterAPIContext.cs
@@ -17,7 +17,7 @@ namespace AnimalShelterAPI.Models
         .HasData(
           
           new Animal {
-            AnimalId = 249017, 
+            AnimalId = 1, 
             Name = "Domino",
             Type = Type.Cat, 
             Breed = "Domestic Medium Hair",
@@ -31,7 +31,7 @@ namespace AnimalShelterAPI.Models
             },
 
             new Animal {
-            AnimalId = 249281, 
+            AnimalId = 2, 
             Name = "Doug", 
             Type = Type.Cat,
             Breed = "Domestic Short Hair",
@@ -45,7 +45,7 @@ namespace AnimalShelterAPI.Models
             },
 
             new Animal {
-            AnimalId = 249318, 
+            AnimalId = 3, 
             Name = "Bunny",
             Type = Type.Cat, 
             Breed = "Domestic Short Hair",
@@ -59,7 +59,7 @@ namespace AnimalShelterAPI.Models
             },
 
             new Animal {
-            AnimalId = 248680, 
+            AnimalId = 4, 
             Name = "Angus", 
             Type = Type.Dog,
             Breed = "Labrador",
@@ -73,7 +73,7 @@ namespace AnimalShelterAPI.Models
             },
 
             new Animal {
-            AnimalId = 227248, 
+            AnimalId = 5, 
             Name = "Penny", 
             Type = Type.Dog,
             Breed = "American Pit Bull - Australian Shepherd",
@@ -87,7 +87,7 @@ namespace AnimalShelterAPI.Models
             },
 
             new Animal {
-            AnimalId = 249396, 
+            AnimalId = 6, 
             Name = "Churro", 
             Type = Type.Dog,
             Breed = "Dachsund",
@@ -97,6 +97,104 @@ namespace AnimalShelterAPI.Models
             Weight = 15,
             Personality = Personality.Grumpy,
             About = "Love to Learn and Love to Cuddle!",
+            Adopted = false,
+            },
+
+            new Animal {
+            AnimalId = 7, 
+            Name = "Milo", 
+            Type = Type.Dog,
+            Breed = "Cattle",
+            Sex = Sex.Female,
+            Coloring = "Black White",
+            Age = 3,
+            Weight = 50,
+            Personality = Personality.Energetic,
+            About = "Fun fun fun!",
+            Adopted = false,
+            },
+
+            new Animal {
+            AnimalId = 8, 
+            Name = "Mitsy", 
+            Type = Type.Cat,
+            Breed = "Calico",
+            Sex = Sex.Female,
+            Coloring = "Black White Orange",
+            Age = 5,
+            Weight = 15,
+            Personality = Personality.Grumpy,
+            About = "Wild heart!",
+            Adopted = false,
+            },
+
+            new Animal {
+            AnimalId = 9, 
+            Name = "Brutus", 
+            Type = Type.Dog,
+            Breed = "Hound",
+            Sex = Sex.Male,
+            Coloring = "Brown White",
+            Age = 7,
+            Weight = 75,
+            Personality = Personality.Adventerous,
+            About = "I love to explore!",
+            Adopted = false,
+            },
+
+            new Animal {
+            AnimalId = 10, 
+            Name = "Baron", 
+            Type = Type.Dog,
+            Breed = "Collie",
+            Sex = Sex.Male,
+            Coloring = "Black Tan",
+            Age = 8,
+            Weight = 55,
+            Personality = Personality.Kind,
+            About = "Follow my leader!",
+            Adopted = false,
+            },
+
+            new Animal {
+            AnimalId = 11, 
+            Name = "Brew", 
+            Type = Type.Dog,
+            Breed = "Mix",
+            Sex = Sex.Female,
+            Coloring = "Golden",
+            Age = 1.5,
+            Weight = 35,
+            Personality = Personality.Sweet,
+            About = "Lets play!",
+            Adopted = false,
+            },
+
+            new Animal {
+            AnimalId = 12, 
+            Name = "Midge", 
+            Type = Type.Cat,
+            Breed = "Tabby",
+            Sex = Sex.Male,
+            Coloring = "Brown Grey",
+            Age = 5,
+            Weight = 22,
+            Personality = Personality.Sassy,
+            About = "Shop cat!",
+            Adopted = false,
+            },
+
+            new Animal {
+            AnimalId = 13, 
+            Name = "Chubs", 
+            Type = Type.Dog,
+            Breed = "Chow",
+            Sex = Sex.Male,
+            Coloring = "Grey",
+            Age = 6.5,
+            Weight = 65,
+            Personality = Personality.Playful,
+            About = "Throw the ball please!",
             Adopted = false,
             }
         );
