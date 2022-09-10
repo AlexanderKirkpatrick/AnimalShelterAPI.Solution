3b21d705bd6e0912598cd1d8c928eda22b76615c
Alexander <knibz@protonmail.com>
2022-09-10 18:24:11 -0400

    enable swagger

--- AnimalShelterAPI/Startup.cs
+++ AnimalShelterAPI/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using AnimalShelterAPI.Models;
 
 namespace AnimalShelterAPI
@@ -29,6 +30,24 @@ namespace AnimalShelterAPI
             services.AddDbContext<AnimalShelterAPIContext>(opt =>
                 opt.UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "AnimalShelterApi", Version = "v1" });
+            });
+            services.AddSwaggerDocument(config =>
+            {
+                config.PostProcess = document =>
+                {
+                    document.Info.Version = "v1";
+                    document.Info.Title = "Animal Shelter API";
+                    document.Info.Description = "An API for lists of available cats and dogs at a shelter.";
+                    document.Info.Contact = new NSwag.OpenApiContact
+                    {
+                        Name = "Alex Kirkpatrick",
+                        Url = "https://github.com/AlexanderKirkpatrick"
+                    };
+                };
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -36,6 +55,8 @@ namespace AnimalShelterAPI
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseOpenApi();
+                app.UseSwaggerUi3();
             }
 
             // app.UseHttpsRedirection();
